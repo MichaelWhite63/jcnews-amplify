56f9c9cc4245c4063a5642d3e9369760d8b69ba5
Michael White <michaelwhite63@gmail.com>
2026-05-13 05:51:30 -0400

    Extend API key expiry to 365 days

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -75,7 +75,7 @@ export const data = defineData({
   authorizationModes: {
     defaultAuthorizationMode: 'apiKey',
     apiKeyAuthorizationMode: {
-      expiresInDays: 30,
+      expiresInDays: 365,
     },
   },
 });
